ca13486362c184206e6f154c0d5e37ef5080399f
Eishal <eishal.cecos@gmail.com>
2025-03-05 20:02:34 +0500

    pop
    pop

--- pop.js
+++ pop.js
@@ -1,43 +1,45 @@
-const viewBtn document.querySelector(".view-modal"),
+const viewBtn= document.querySelector(".view-modal"),
 
-popup document.querySelector(".popup"),
+popup =document.querySelector(".popup"),
 
-close popup.querySelector(".close"),
+close =popup.querySelector(".close"),
 
-field popup.querySelector(".field"),
+field =popup.querySelector(".field"),
 
-input field.querySelector("input"),
+input =field.querySelector("input"),
 
-copy field.querySelector("button");
+copy= field.querySelector("button");
 
-viewBtn.onclick ()={
+viewBtn.onclick= ()=>{
 
 popup.classList.toggle("show");
 
 }
 
-close.onclick = (){
+close.onclick = ()=>{
 
 ViewBtn.click();
 
 }
 
-copy.onclick = ()(
+copy.onclick = ()=>{
 
-input.select(); //select saput value
+input.select();
 
-if(document.execCommand("copy")) { // the selected text is copied
+if(document.execCommand("copy")) { 
 
 field.classList.add("active");
 
 copy.innerText = "Copied";
 
-setTimeout((){
+setTimeout(()=>{
 
-window.getSelection().removeAllRanges(); //remove setection from page
+window.getSelection().removeAllRanges(); 
 
 field.classList.remove("active");
 
 copy.innerText = "Copy";
 
 }, 3000);
+}
+}
